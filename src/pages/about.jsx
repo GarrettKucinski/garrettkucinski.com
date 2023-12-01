89b5f537e32409e818b5800f4ee9cef6e39e7c9f
Garrett Kucinski <garrett.kucinski@gmail.com>
2023-12-01 11:57:52 -0700

    fix pre tag

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -43,7 +43,7 @@ export default function About() {
         <title>About - Garrett Kucinski</title>
         <meta
           name="description"
-          content="I’m Garrett Kucinski. where I build awesome things for the space industry and write about engineering."
+          content="I’m Garrett Kucinski. I write about entrepreneurship, productivity and lifestyle."
         />
       </Head>
       <Container className="mt-16 sm:mt-32">
@@ -60,10 +60,8 @@ export default function About() {
           </div>
           <div className="lg:order-first lg:row-span-2">
             <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-              <pre>
-                I’m Garrett Kucinski.
-                I write about entrepreneurship, productivity and lifestyle.
-              </pre>
+              I’m Garrett Kucinski.<br />
+              I write about entrepreneurship, productivity and lifestyle.
             </h1>
             <div className="mt-6 text-base space-y-7 text-zinc-600 dark:text-zinc-400">
               <p>
